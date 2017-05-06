781a50b5fec0f39d492aea6e4c8fbd8a0d118a59
Karg Weng <kargweng@gmail.com>
2017-05-06 21:28:54 +0800

    Simple webpack config with dependencies and devdependencies

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,18 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+          { 
+            test: /\.jsx?$/, 
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            options: {
+                plugins: ['transform-runtime'],
+                presets: ['es2015']
+            }
+          }
+      ]
     }
 };
